0009a3825d8f7c98245b06198e5024191856ccae
SeoungMoJeoung <64403209+SeoungMoJeoung@users.noreply.github.com>
2020-09-10 14:41:34 +0900

    Update BIO_04_linear_model_RelativeData_Extract.py

--- BIO/BIO_04_linear_model_RelativeData_Extract.py
+++ BIO/BIO_04_linear_model_RelativeData_Extract.py
@@ -1,3 +1,8 @@
+# 만든이 : 정성모
+# 입력 : raw193.csv
+# 출력 : Original data, Relative_data
+# block_bio_queue, block_getrq, nvme_sq를 상대시간으로 변경
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -17,7 +22,7 @@ def main():
 	data['block_getrq']= data['block_getrq'] - data['block_bio_queue']
 	bio_queue_preprocessing(data)
 
-#	data.to_csv("relative_data.csv", mode='w')
+#	data.to_csv("Relative_data.csv", mode='w')
 
 # bio_queue의 해당 row의 값은 해당 row값에서 첫번째 row 값에서 뺀 값, 첫번째 row 값은 0
 def bio_queue_preprocessing(x):
