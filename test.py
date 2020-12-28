b81fb18d3c510d272aa6a592cf259b998b1e0ada
SeongMo Jeong <64403209+SeoungMoJeoung@users.noreply.github.com>
2020-12-28 21:42:04 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel("./eventdata.xlsx")
-data = data['GPS포인트집합']
-data.dropna(how="all", inplace=True)
-data.reset_index(drop=True, inplace=True)
-data = data.head(10)
-print(data)
-data.to_excel("pre_data_test.xlsx")
